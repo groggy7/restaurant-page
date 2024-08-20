e31d7a8293df789eff82f3a55e7acad13c373ae7
Serhat Kılbaş <serhatkilbas127@gmail.com>
2024-08-20 22:42:19 +0300

    Add header and navigation bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+import "./style.css";
+
+const navs = document.querySelectorAll("nav");
+const content = document.getElementById("content");
+navs[0].innerText = "Home";
+navs[1].innerText = "Menu";
+navs[2].innerText = "About";
+
+const h1 = document.createElement("h1");
+h1.innerText = "KEBAB RESTAURANT";
+
+content.appendChild(h1);
